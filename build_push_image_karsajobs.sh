71503f4268ff53f6b06812aafdd557e1d2359fdc
Gilang Heavy <gilangheavy@gmail.com>
2025-11-30 11:12:24 +0700

    chore: setup ci pipeline

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Variabel (Sesuaikan username GitHub lo)
+IMAGE_NAME="karsajobs"
+TAG="latest"
+GITHUB_USER="gilangheavy"
+GHCR_IMAGE="ghcr.io/$GITHUB_USER/$IMAGE_NAME:$TAG"
+
+# 1. Build Image
+echo "Building Docker Image..."
+docker build -t $GHCR_IMAGE .
+
+# 2. Login GHCR
+echo "Logging in to GitHub Packages..."
+echo $CR_PAT | docker login ghcr.io -u $GITHUB_USER --password-stdin
+
+# 3. Push Image
+echo "Pushing Image to GHCR..."
+docker push $GHCR_IMAGE
